e4c3f7adab8e6e501637cd44a69c2e9138dd9b42
GuuD WU <guuuuud@aliyun.com>
2015-07-07 16:18:03 +0800

    Add block in "script/gitconfig.sh"
    Move existing file into backup directory.

--- script/gitconfig.sh
+++ script/gitconfig.sh
@@ -2,8 +2,14 @@
 
 if [ ! -d "backup" ]
 then
-  echo 'backup directory must exist'
+  echo 'Backup directory must exist.'
   exit 255
+fi
+
+if [ -f $HOME"/.gitconfig" ]
+then
+  echo 'Existing file moved to backup directory.'
+  mv $HOME"/.gitconfig" backup/
 fi
 
 exit 0
