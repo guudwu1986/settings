1c691b6313218a9d59e9c82a8e2ffa521df35eed
GuuD WU <guuuuud@aliyun.com>
2015-07-13 20:10:50 +0800

    Add block in "script/bashrc.sh"
    Move existing file to backup directory.

--- script/bashrc.sh
+++ script/bashrc.sh
@@ -6,4 +6,11 @@ then
   exit 255
 fi
 
+current_time=$(date +%Y%m%d-%H%M%S)
+if [ -d $HOME"/.bashrc" -o -f $HOME"/.bashrc" ]
+then
+  echo 'Existing file moved to backup directory.'
+  mv $HOME"/.bashrc" backup/bashrc-$current_time
+fi
+
 exit 0
